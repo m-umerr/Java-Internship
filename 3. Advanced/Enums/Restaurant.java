fa7e58b3cd4c3b566f6b95f5e5b91e26076274d0
Muhammad Umer <mumerr03@gmail.com>
2024-07-10 21:51:43 +0500

    Sample program using Enum

---
+++ 3. Advanced/Enums/Restaurant.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public class Restaurant {
+    public static void main(String[] args) {
+        FoodOrder foodOrder = new FoodOrder();
+        foodOrder.orderFood(Beverage.DRINK);
+    }
+}
