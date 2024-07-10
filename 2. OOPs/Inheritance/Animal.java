f8b615619c92e3f45941e4fa609788c1e2a0cab0
Muhammad Umer <mumerr03@gmail.com>
2024-07-10 22:01:50 +0500

    Sample program using Inheritance

---
+++ 2. OOPs/Inheritance/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String name;
+    
+    public Animal(String name) {
+        this.name = name;
+    }
+    
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+    
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+}
