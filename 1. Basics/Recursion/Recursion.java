b54a2315a765ba1d880771aa9f122b97b79d1863
Muhammad Umer <mumerr03@gmail.com>
2024-07-10 21:12:54 +0500

    Fibonacci sequence using recursion

---
+++ 1. Basics/Recursion/Recursion.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Recursion {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n){
+        if(n<=1){
+            return n;
+        }
+        return (fibonacci(n-1)+fibonacci(n-2));
+    }
+}
