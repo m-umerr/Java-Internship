bb1d070a0692fe6753bf1de194cd56f1e82c9a4d
Muhammad Umer <mumerr03@gmail.com>
2024-07-10 20:53:11 +0500

    Simple array program

---
+++ 1. Basics/Arrays/Array.java
@@ -0,0 +1,13 @@
+public class Array {
+
+    public static void main(String[] args) {
+        int[][] arr = new int[5][5];
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                arr[i][j]=i;
+                System.out.print(" | "+arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
