609edd79cfe788c1cf6513f6f8537555943cf48a
Muhammad Umer <mumerr03@gmail.com>
2024-07-11 10:28:00 +0500

    Generics Sample Program

--- 3. Advanced/Generics/Generics.java
+++ 3. Advanced/Generics/Generics.java
@@ -1,1 +1,22 @@
+package Generics;
 
+public class Generics {
+    public static void main(String[] args) {
+
+        Box<String> box = new Box<>("Generics");
+        System.out.println(box.getValue());
+        System.out.println(box.container.getClass().getName());
+
+        Box<Integer> box1 = new Box<>(121);
+        System.out.println(box1.getValue());
+        System.out.println(box1.container.getClass().getName());
+
+        box.container = "Generics Sample Program";
+        box1.container = 1452;
+        Box<Boolean> box2 = new Box<>(true);
+        System.out.println(box2.getValue());
+        box.performSomeTask();
+        box1.performSomeTask();
+
+    }
+}
