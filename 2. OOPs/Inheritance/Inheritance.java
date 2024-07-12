aeb1ec1fef70a6c0e784c5a99ceca2a9aafd9758
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 14:13:03 +0500

    Sample Program on Inheritance

--- 2. OOPs/Inheritance/Inheritance.java
+++ 2. OOPs/Inheritance/Inheritance.java
@@ -1,1 +1,61 @@
+package Inheritance;
 
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking...");
+    }
+}
+
+class BabyDog extends Dog {
+    void weep() {
+        System.out.println("BabyDog is weeping...");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat is meowing...");
+    }
+}
+
+interface Pet {
+    void play();
+}
+
+class Poodle extends Dog implements Pet {
+    public void play() {
+        System.out.println("Poodle is playing...");
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        // Single Inheritance
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+        // Multilevel Inheritance
+        BabyDog babyDog = new BabyDog();
+        babyDog.eat();
+        babyDog.bark();
+        babyDog.weep();
+
+        // Hierarchical Inheritance
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+
+        // Multiple Inheritance
+        Poodle poodle = new Poodle();
+        poodle.eat();
+        poodle.bark();
+        poodle.play();
+    }
+}
