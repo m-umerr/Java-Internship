9a7e68f1da983ecf904e8588fb18d73da92db596
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 10:40:48 +0500

    Sample Program on Lambda Expressions

--- 3. Advanced/LambdaExpressions/LambdaExpressions.java
+++ 3. Advanced/LambdaExpressions/LambdaExpressions.java
@@ -1,1 +1,22 @@
+package LambdaExpressions;
 
+public class LambdaExpressions {
+
+    public static void main(String[] args) {
+        // Traditional anonymous class implementation
+        Runnable runnable1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello from traditional Runnable!");
+            }
+        };
+
+        // Lambda expression implementation
+        Runnable runnable2 = () -> {
+            System.out.println("Hello from lambda Runnable!");
+        };
+
+        runnable1.run();
+        runnable2.run();
+    }
+}
