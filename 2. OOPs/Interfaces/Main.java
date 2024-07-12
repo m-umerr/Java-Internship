2dff1e8bbd7510198b319c5d23c480c8ea297176
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 14:19:15 +0500

    Sample Program on Interfaces

---
+++ 2. OOPs/Interfaces/Main.java
@@ -0,0 +1,14 @@
+package Interface;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        System.out.println("Area of circle: " + circle.calculateArea());
+        System.out.println("Perimeter of circle: " + circle.calculatePerimeter());
+
+        System.out.println("Area of rectangle: " + rectangle.calculateArea());
+        System.out.println("Perimeter of rectangle: " + rectangle.calculatePerimeter());
+    }
+}
