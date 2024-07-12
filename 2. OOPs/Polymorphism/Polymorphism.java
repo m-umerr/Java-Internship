6f27e5c34dd4dfdb85ff8d3c9d3f49e2d6f0ba42
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 14:15:16 +0500

    Sample Program on Polymorphism

--- 2. OOPs/Polymorphism/Polymorphism.java
+++ 2. OOPs/Polymorphism/Polymorphism.java
@@ -1,1 +1,17 @@
+package Polymorphism;
 
+public class Polymorphism {
+    public static void main(String[] args) {
+        display(5);
+        display("Hello");
+    }
+
+    // Method overloading
+    static void display(int num) {
+        System.out.println("Integer number: " + num);
+    }
+
+    static void display(String str) {
+        System.out.println("String: " + str);
+    }
+}
