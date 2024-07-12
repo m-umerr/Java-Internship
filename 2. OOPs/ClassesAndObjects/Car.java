4e12627c59c28ba2acf2ad79e41a1e24156b02a2
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 14:26:15 +0500

    Sample Program on Classes and Objects

---
+++ 2. OOPs/ClassesAndObjects/Car.java
@@ -0,0 +1,19 @@
+package ClassesAndObjects;
+
+class Car {
+    String brand;
+    String model;
+    int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Year: " + year);
+    }
+}
