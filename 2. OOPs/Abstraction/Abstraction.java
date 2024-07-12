75e4bf74e50bcb79de733287d3f406462159d302
Muhammad Umer <mumerr03@gmail.com>
2024-07-12 14:10:05 +0500

    Sample Program on Abstraction

--- 2. OOPs/Abstraction/Abstraction.java
+++ 2. OOPs/Abstraction/Abstraction.java
@@ -1,1 +1,35 @@
+package Abstraction;
 
+abstract class Shape {
+
+    public abstract double calculateArea();
+
+    public void display() {
+        System.out.println("Displaying shape");
+    }
+
+}
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+}
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+
+        circle.display();
+
+        System.out.println("Area of circle: " + circle.calculateArea());
+
+    }
+}
